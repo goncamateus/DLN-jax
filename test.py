5b7e4cb8d38a944381e1248b3aaa0de28cc0b48e
goncamateus <mgm4@cin.ufpe.br>
2024-05-31 10:59:20 -0300

    Creating test script

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ import numpy as np
 import orbax.checkpoint
 
 from PIL import Image
+
 from torchvision import transforms
 from tqdm import tqdm
 
 
-from dln.dataset import is_image_file
 from dln.model import DLN
 
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
@@ -20,19 +20,6 @@ os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 def parse_args():
     parser = argparse.ArgumentParser(description="PyTorch Super Res Example")
-    parser.add_argument(
-        "--upscale_factor", type=int, default=1, help="super resolution upscale factor"
-    )
-    parser.add_argument("--chop-forward", type=bool, default=True)
-    parser.add_argument(
-        "--patch-size", type=int, default=256, help="0 to use original frame size"
-    )
-    parser.add_argument(
-        "--stride", type=int, default=16, help="0 to use original patch size"
-    )
-    parser.add_argument(
-        "--seed", type=int, default=123, help="random seed to use. Default=123"
-    )
     parser.add_argument("--gpus", default=1, type=int, help="number of gpu")
     parser.add_argument("--image-dataset", type=str, default="test_img")
     parser.add_argument(
@@ -43,13 +30,15 @@ def parse_args():
         default="DLN-MODEL",
         help="sr pretrained base model",
     )
-    parser.add_argument(
-        "--image-based", type=bool, default=True, help="use image or video based ULN"
-    )
-    parser.add_argument("--chop", type=bool, default=False)
 
     args = parser.parse_args()
     return args
+
+
+def is_image_file(filename):
+    return any(
+        filename.endswith(extension) for extension in [".bmp", ".png", ".jpg", ".jpeg"]
+    )
 
 
 def image_paths(data_path, out_path):
@@ -74,21 +63,44 @@ def image_paths(data_path, out_path):
 
 
 @jax.jit
-def eval_step(state, X):
-    nl_pred = DLN(dim=64).apply({"params": state.params}, X)
+def eval_step(params, X):
+    nl_pred = DLN(dim=64).apply({"params": params}, X)
     return nl_pred
 
 
-def eval_over_images(LL_images, pred_images, state):
+def eval_over_images(LL_images, pred_images, model_params):
+    time_ave = 0
     trans = transforms.ToTensor()
     for i in tqdm(range(len(LL_images))):
         LL_in = Image.open(LL_images[i]).convert("RGB")
         LL_torch = trans(LL_in).permute(1, 2, 0).unsqueeze(0)
         LL = jnp.array(LL_torch)
-        print(LL.shape)
+        t0 = time.time()
+        nl_pred = eval_step(model_params, LL)
+        t1 = time.time()
+        time_ave += t1 - t0
+        # save images
+        nl_pred = nl_pred * 255.0
+        nl_pred = nl_pred.clip(0, 255)
+        Image.fromarray(np.uint8(nl_pred)).save(pred_images[i])
+        print(
+            "===> Processing Image: %04d /%04d in %.4f s."
+            % (i, len(LL_images), (t1 - t0))
+        )
+    print("===> Processing Time: %.4f ms." % (time_ave / len(LL_images) * 1000))
+
+
+def load_model_params(model_folder):
+    orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
+    indexes_in_folder = sorted([int(i) for i in os.listdir(model_folder)])
+    model_folder = f"{model_folder}/{indexes_in_folder[-1]}/default"
+    chkpt = orbax_checkpointer.restore(model_folder)
+    model_params = chkpt["model"]["params"]
+    return model_params
 
 
 if __name__ == "__main__":
     args = parse_args()
+    model_params = load_model_params(args.model_folder)
     LL_images, pred_images = image_paths(args.image_dataset, args.output)
-    eval_over_images(LL_images, pred_images, None)
+    eval_over_images(LL_images, pred_images, model_params)
